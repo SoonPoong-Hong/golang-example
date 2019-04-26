f9fa440b108b5351430ae84f605c40567ef355b2
Hong SoonPoong <rocklike@nexon.co.kr>
2019-04-26 10:48:39 +0900

    .

--- rocklike/test/honghello/hongHello.go
+++ rocklike/test/honghello/hongHello.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"golang-example/rocklike/test/helloutil"
+	"math"
 )
+
+func testMath() {
+	fmt.Println(math.Pi)
+}
+
+func add(a int, b int) int {
+	return a + b
+}
 
 func main() {
 	s := "012345"
-	fmt.Println(helloutil.Reverse(s))
+	s = helloutil.Reverse(s)
+	testMath()
+	fmt.Print(add(10, 20))
 }
